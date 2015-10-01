bda4f984c0372c6a3fe76bca183d3d19a4fe9d22
Khải <hvksmr1996@gmail.com>
2015-10-01 16:05:08 +0700

    Update devtools.js

--- devtools.js
+++ devtools.js
@@ -1,5 +1,9 @@
-var xhr = new XMLHttpRequest();
+/* var xhr = new XMLHttpRequest();
 xhr.open("GET", "/styles.css", false);
 xhr.send();
-chrome.devtools.panels.applyStyleSheet(xhr.responseText);
+chrome.devtools.panels.applyStyleSheet(xhr.responseText); */
 
+function main() {
+  'use strict';
+  // TODO: load a theme by the information stored in chrome.store
+}
